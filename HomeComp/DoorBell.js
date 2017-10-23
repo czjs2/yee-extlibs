7f42b6af54dbdd13457a325390a5d2d99de1afd8
祝启忠 <pluto.shu@gmail.com>
2017-10-23 18:40:34 +0800

    添加两个库

---
+++ HomeComp/DoorBell.js
@@ -0,0 +1,46 @@
+/**
+ * Created by zhuqizhong on 17-10-11.
+ */
+
+var Component = require('OStars/Core/Base').Component;
+const _ = require('lodash');
+const NotifySound = {
+    STAGE1:26,
+    STAGE2:29
+}
+/**
+ *
+ * 输入   {} 一个对象
+ *
+ */
+class DoorBell extends Component{
+    constructor(store_info, manager){
+        store_info.inputs = [{"type": 'object'}];
+        store_info.outputs = [{"type": 'object'},{"type":"object"}];
+        store_info.config_setting =[{name:"midVal",type:"Number"}]
+        super(store_info,manager);
+    }
+
+    updateLogical(pin){
+        if(pin === 0 ){
+            let input_val = this.getInputLatchVal(0) ;
+            if(input_val){
+                if(!this.timeHandler){
+                    this.timeHandler = setTimeout(()=>{
+                        this.setOutLatchVal(0,10);
+                        this.setOutLatchVal(1,false);
+                        this.updateOutput();
+                        this.timeHandler = null;
+                    },4000);
+                    this.setOutLatchVal(0,10);
+                    this.setOutLatchVal(1,true);
+                }
+            }else{
+
+            }
+        }
+    }
+
+}
+
+module.exports = DoorBell;
